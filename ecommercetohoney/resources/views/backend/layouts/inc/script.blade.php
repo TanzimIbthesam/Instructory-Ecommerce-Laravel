4b4ef9ec62cc441a8867acea41a32bfbae04e38f
TanzimIbthesam <tanzim67@gmail.com>
2022-08-09 15:09:58 +0600

    3-Scripts removed not needed now

--- ecommercetohoney/resources/views/backend/layouts/inc/script.blade.php
+++ ecommercetohoney/resources/views/backend/layouts/inc/script.blade.php
@@ -1,11 +1,7 @@
 <script src="{{asset('assets/backend')}}/js/vendor/jquery-3.5.1.min.js"></script>
     <script src="{{asset('assets/backend')}}/js/vendor/bootstrap.bundle.min.js"></script>
     <script src="{{asset('assets/backend')}}/js/vendor/OverlayScrollbars.min.js"></script>
-    <script src="{{asset('assets/backend')}}/js/vendor/autoComplete.min.js"></script>
-    <script src="{{asset('assets/backend')}}/js/vendor/clamp.min.js"></script>
-    <script src="{{asset('assets/backend')}}/js/vendor/Chart.bundle.min.js"></script>
-    <script src="{{asset('assets/backend')}}/js/vendor/chartjs-plugin-rounded-bar.min.js"></script>
-    <script src="{{asset('assets/backend')}}/js/vendor/jquery.barrating.min.js"></script>
+
     <!-- Vendor Scripts End -->
 
     <!-- Template Base Scripts Start -->
@@ -18,7 +14,4 @@
     <script src="{{asset('assets/backend')}}/js/base/init.js"></script>
     <!-- Template Base Scripts End -->
     <!-- Page Specific Scripts Start -->
-    <script src="{{asset('assets/backend')}}/js/cs/charts.extend.js"></script>
-    <script src="{{asset('assets/backend')}}/js/pages/dashboard.js"></script>
-    <script src="{{asset('assets/backend')}}/js/common.js"></script>
-    <script src="{{asset('assets/backend')}}/js/scripts.js"></script>
+
